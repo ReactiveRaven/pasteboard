8a6af20df891524526a75b1b68f19ce90aaf8167
David Godfrey <reactiveraven@gmail.com>
2013-06-03 17:25:42 +0100

    proof of concept

--- popup.js
+++ popup.js
@@ -1,13 +1,28 @@
-function copy(){
-
-    //Get Input Element
-    document.getElementById("text").select();
-
-    //Copy Content
+$(function () {
+  var copy = function (event, text) {
+    var $input = $("#textToCopy").first();
+    $input.val(text);
+    $input.select();
     document.execCommand("Copy", false, null);
-}
+  }
+  
+  $("body").on("doCopy", copy);
+});
 
-//Add Event Listeners to Button Click
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("copy").onclick = copy;
+$(function () {
+  var faces = [
+    ":)",
+    ":(",
+    ":|",
+    "o.o?",
+    ">.<'"
+  ];
+  
+  for (var i = 0; i < faces.length; i++) {
+    $("body").append("<div class=\"face\">" + faces[i] + "</div>");
+  }
+  
+  $(".face").on("click", function () {
+    $("body").trigger("doCopy", [$(this).text()]);
+  })
 });
